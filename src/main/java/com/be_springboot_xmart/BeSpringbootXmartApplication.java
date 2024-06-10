fff8b8fa93038c96c1d8c812c194c5e56739d4f0
kemalmahmud <kemalmahmud93@gmail.com>
2024-06-10 15:46:36 +0700

    first push

---
+++ src/main/java/com/be_springboot_xmart/BeSpringbootXmartApplication.java
@@ -0,0 +1,13 @@
+package com.be_springboot_xmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringbootXmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringbootXmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be_springboot_xmart/BeSpringbootXmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be_springboot_xmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringbootXmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
